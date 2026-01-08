25535b786df269ce09d72295a60bfb12271747be
Kunal-Rathore <kunalworkspace111@gmail.com>
2026-01-08 17:39:39 +0530

    updated the BLOGS_PLATFORM_URL from landing page redirect to home page redirect

--- src/config/api.js
+++ src/config/api.js
@@ -5,6 +5,6 @@
 export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
 
 export const BLOGS_API_URL = "https://blogs-platform-g6ej.vercel.app/fetch-posts";
-export const BLOGS_PLATFORM_URL = "https://blogs-platform-tawny.vercel.app/";
+export const BLOGS_PLATFORM_URL = "https://blogs-platform-tawny.vercel.app/home";
 
 
